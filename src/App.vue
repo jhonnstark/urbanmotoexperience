d75b9a496b46f663eaf1675441946c0d5bf82e6e
Jonathan Cervantes <72222544+jhonnstark@users.noreply.github.com>
2026-05-22 00:20:07 -0600

    feat: create main app component with theme and i18n support

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-app>
+    <v-app-bar color="primary" dark>
+      <v-app-bar-title>{{ $t('app.title') }}</v-app-bar-title>
+      <v-spacer />
+      <v-btn icon @click="toggleTheme">
+        <v-icon>mdi-palette</v-icon>
+      </v-btn>
+      <v-menu>
+        <template #activator="{ props }">
+          <v-btn icon v-bind="props">
+            <v-icon>mdi-translate</v-icon>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item @click="$i18n.locale = 'en'">
+            <v-list-item-title>English</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="$i18n.locale = 'fr'">
+            <v-list-item-title>Français</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+
+    <v-footer app>
+      <span>{{ $t('app.footer') }} © 2026</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+const toggleTheme = () => {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
+
+<style scoped>
+</style>
